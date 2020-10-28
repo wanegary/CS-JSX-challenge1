9da7f88e26f002ade63cf7f85a216fe43f7fd3b1
wanegary <66226401+wanegary@users.noreply.github.com>
2020-10-27 23:05:50 -0400

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h1>The Sports I Love</h1>
+    <ul>
+      <li>Football</li>
+      <li>Baseball</li>
+      <li>Hockey</li>
+    </ul>
+  </div>,
+  document.getElementById("root")
+);
